3955faa318169bd537e55a3ba820db8eb18920e0
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-08-12 22:58:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useState } from 'react';
 
+
+function App() {
+  const [dark,setdark] = useState(false)
+  return(
+      <div className="App"  style={ dark ? {backgroundColor:"black",color:"white"} : {backgroundColor:"White",color:"black"}}>
+      <button on onClick={()=>
+      setdark(!dark)
+      }>click me</button>
+      <br></br>
+          <h3 style={{color :"green" }}>
+            come waste time 
+          </h3>
+          <br></br>
+
+          <h3 style={{color :"green" }}>witth me</h3>
+          <br></br>
+          <br></br>
+            <h1 style={{color :"green" }}>THE WET CODE BASE</h1>
+            <br></br>
+          <br></br>
+          <br></br>
+          <h3 style={{color :"green" }}>
+            come waste time 
+          </h3>
+          <br></br> <br></br>
+          <br></br>
+          <h3 style={{color :"green" }}>
+            come waste time with me
+          </h3>
+          <br></br>
+          <h3 style={{color :"green" }}>
+            come waste time with me
+          </h3>
+          <br></br>
+          <h1 style={{color :"green" }}>GOOD BYE,CLEAN CODE</h1>
+          <br></br>
+          <br></br>
+          <h3  style={{color :"green" }}>
+            come waste time
+          </h3>
+          <br></br>
+          <h3 style={{color :"green" }}>
+            come waste time with me
+          </h3>
+          <br></br>
+          <h1 style={{color :"green" }}>MY DECADE IN REVIEW</h1>
+
+      </div>
+    );
+}
 export default App;
